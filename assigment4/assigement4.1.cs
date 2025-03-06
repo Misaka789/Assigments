8a5cffc41cd93c5a86078bf50fb561fe7befab1c
misaka <3192422357@qq.com>
2025-03-06 19:27:06 +0800

    “assigemnt4.1”

---
+++ assigment4/assigement4.1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+
+namespace assigment4
+{
+    internal class assigment4_1
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>(12,3,4,5,5);
+            list.ForEach(t => Console.Write(t+" "));
+            Console.WriteLine();
+            int sum = 0;int max = int.MinValue;int min = int.MaxValue;
+            list.ForEach(t => sum += t);
+            Console.WriteLine();
+            list.ForEach(t => max = max > t ? max : t);
+            list.ForEach(t => min = min > t ? t : min);
+            Console.WriteLine($"list链表最大值为{max}");
+            Console.WriteLine();
+            Console.WriteLine($"list链表最小值为{min}");
+        }
+    }
+}
